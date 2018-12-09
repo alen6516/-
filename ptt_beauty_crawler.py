254b3f7a110d396133262aecc36dcf9ef24b80e0
alen6516 <alen6516@gmail.com>
2018-12-09 22:54:31 +0800

    modify

--- ptt_beauty_crawler.py
+++ ptt_beauty_crawler.py
@@ -5,9 +5,16 @@
 #####
 import re, sys, os, errno
 import requests
+requests.packages.urllib3.disable_warnings()
 from bs4 import BeautifulSoup
 from urllib.request import urlretrieve
 import threading
+
+DEBUG = 1
+
+def msg(text):
+    if DEBUG == 1:
+        print(str(text))
 
 
 class Beauty_crawler():
@@ -59,9 +66,7 @@ class Beauty_crawler():
 
     def download(self, target):
         res=requests.get(target, verify=False)
-        soup = BeautifulSoup(res.text)
-        pa1='https?://i.imgur\..+'
-        pa2='https?://imgur\..+'
+        soup = BeautifulSoup(res.text, "html.parser")
 
         title = self._get_title(soup)
         post_time=self._get_post_time(soup)
@@ -69,55 +74,49 @@ class Beauty_crawler():
         self.makedir(path)
 
         for a in soup.find_all('a', href=True):
-            str=a['href']
-            ans1=re.findall(pa1, str)
-            ans2=re.findall(pa2, str)
-            if ans1:
-                #print("ans1=%s" % ans1)
-                for url in ans1:
-                    if url[-3:]=='jpg' or url[-3:]=='gif' or url[-3:]=='png' or url[-3:]=='peg':
-                    
-                        file_path=path+url.split('/')[-1]
-                        #print(url)
-                        #urlretrieve(url, path+url.split('/')[-1])
-                        self._write(file_path, url)
-                    else:
-                        url="https://i."+url.split('https://')[1]+'.jpg'
-                        file_path=path+url.split('/')[-1]
-                        #print(url)
-                        #urlretrieve(url, path+url.split('/')[-1])
-                        self._write(file_path, url)
-            if ans2:
-                #print("ans1=%s" % ans1)
-                for url in ans2:
-                    if url[-3:]=='jpg' or url[-3:]=='gif' or url[-3:]=='jpeg' or url[-3:]=='png':
-                        #print(url)
-                        file_path=path+url.split('/')[-1]
-                        self._write(file_path, url)
-                    else:
-                        url="https://i."+url.split('https://')[1]+'.jpg'
-                        file_path=path+url.split('/')[-1]
-                        #print(url)
-                        #urlretrieve(url, path+url.split('/')[-1])
-                        self._write(file_path, url)
+            res = re.search("href=\".+?\"", str(a)).group()
+            url = res.split("\"")[1]
+            
+            if url.endswith(".jpg") or url.endswith(".jpeg") or url.endswith(".gif"):
+                msg(url)
+                file_path=path+url.split('/')[-1]
+                self._write(file_path, url)
 
 if __name__=='__main__':
     crawler=Beauty_crawler()
+
+    thread_list=[]
+
     op="1"
-    while op!="0":
-        op=input("[0]exit, [1] download, [2] set path, [3] show curr path:\n>>> ")
-        if op=="0":
-            break
-        
-        elif op=="1":
-            target_=input("give the target url:\n")
-            threading.Thread(target=crawler.download, args=(target_,)).start()
 
-        elif op=="2":
-            crawler.set_path()
+    try:
+        while op!="0":
+            op=input("[0]exit, [1] download, [2] set path, [3] show curr path:\n>>> ")
+            if op=="0":
+                for t in thread_list:
+                    if t.is_alive():
+                        print("wait for downloading before close")
+                break
+            
+            elif op=="1":
+                target_=input("give the target url:\n")
+                t = threading.Thread(target=crawler.download, args=(target_,))
+                t.start()
+                thread_list.append(t)
+                for t in thread_list:
+                    if not t.is_alive():
+                        thread_list.remove(t) 
 
-        elif op=="3":
-            print("current download_path:\n%s" % crawler.get_path())
-        
-        else:
-            print("wrong op, try again")
+            elif op=="2":
+                crawler.set_path()
+
+            elif op=="3":
+                print("current download_path:\n%s" % crawler.get_path())
+            
+            else:
+                print("wrong op, try again")
+    except KeyboardInterrupt:
+        for t in thread_list:
+            if t.is_alive():
+                print("wait for downloading before exit")
+
